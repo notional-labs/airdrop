4da9000e865d10fc3b8c5d20fc886ed24057982b
Trung Nguyen Thanh <59964019+trungnt1811@users.noreply.github.com>
2024-07-04 12:03:36 +0700

    refactor logging (#6)
    * refactor logging
    
    * chore: update go.mod and gitignore

--- cmd/airdrop/main.go
+++ cmd/airdrop/main.go
@@ -22,34 +22,39 @@ func main() {
 	// Capture start time
 	startTime := time.Now()
 
+	// Setup logger
 	logger, err := logger.Setup()
 	if err != nil {
 		log.Fatalf("Failed to initialize zap logger: %v", err)
 	}
+	defer logger.Sync() // Flushes buffer, if any
 
+	// Parse command line flags
 	configPath := flag.String("c", "configs/config.toml", "path to config file")
 	blockHeight := flag.String("h", "latest", "block height to query")
 	flag.Parse()
 
+	// Load config
 	cfg, err := config.LoadConfig(*configPath)
 	if err != nil {
 		logger.Fatal("Failed to load config", zap.Error(err))
 	}
 
+	// Determine block height
 	if *blockHeight == "latest" {
-		var err error
 		*blockHeight, err = queries.GetLatestHeight(cfg.RPCServerAddress + "/status")
 		if err != nil {
-			log.Fatal("Failed to fetch latest height", zap.Error(err))
+			logger.Fatal("Failed to fetch latest height", zap.Error(err))
 		}
 	} else {
 		if _, err := strconv.Atoi(*blockHeight); err != nil {
-			log.Fatal("Please provide the block height as an integer")
+			logger.Fatal("Please provide the block height as an integer", zap.Error(err))
 		}
 	}
 
-	logger.Info("", zap.String("Block height", *blockHeight))
+	logger.Info("Using block height", zap.String("Block height", *blockHeight))
 
+	// Setup gRPC connection
 	conn, err := utils.SetupGRPCConnection(cfg.GRPCServerAddress)
 	if err != nil {
 		logger.Fatal("Failed to connect to GRPC server", zap.Error(err))
@@ -58,15 +63,24 @@ func main() {
 
 	client := query.NewQueryClient(conn)
 
+	// Perform airdrop calculation
 	balanceInfo, err := chains.Airdrop(client.StakingClient, *configPath, *blockHeight, logger)
 	if err != nil {
 		logger.Fatal("Failed to calculate airdrop", zap.Error(err))
 	}
 
-	fileBalance, _ := json.MarshalIndent(balanceInfo, "", " ")
-	_ = os.WriteFile("balance.json", fileBalance, 0o600)
+	// Write balance info to file
+	fileBalance, err := json.MarshalIndent(balanceInfo, "", " ")
+	if err != nil {
+		logger.Fatal("Failed to marshal balance info", zap.Error(err))
+	}
 
-	// Calculate and print total time duration
+	err = os.WriteFile("balance.json", fileBalance, 0o600)
+	if err != nil {
+		logger.Fatal("Failed to write balance info to file", zap.Error(err))
+	}
+
+	// Calculate and log total time duration
 	duration := time.Since(startTime)
-	logger.Info("", zap.String("Total time taken", duration.String()))
+	logger.Info("Total time taken", zap.String("Duration", duration.String()))
 }
